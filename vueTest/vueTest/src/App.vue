3d3d59d629da4a0a853270a96e5a07ae1f189fda
goddessIU <2581606394@qq.com>
2022-03-31 21:25:32 +0800

    3.31-2

--- vueTest/vueTest/src/App.vue
+++ vueTest/vueTest/src/App.vue
@@ -1,23 +1,4 @@
-<script setup>
-import { provide } from 'vue';
-import Attrb from './Attrb.vue'
-import { useRouter } from 'vue-router';
-provide('message', 'hello')
-const router = useRouter()
-const toA = () => {
-  router.push({
-    name: 'user'
-  })
-}
 
-
-</script>
-
-<template>
-  <Attrb :modelValue="searchText" @update:modelValue="newValue => searchText = newValue"></Attrb>
-  <button @click="toA">sss</button>
-  <router-view></router-view>
-</template>
 
 <script>
 export default {
@@ -40,5 +21,17 @@ export default {
       console.log(this.$el)
     }
 }
+</script>
+
+<script setup>
+import { readonly, toRaw, reactive } from 'vue'
+let m = reactive({
+  name: 'wang',
+  age: 20
+})
+let n = toRaw(m)
+console.log(n.name)
+m.name = 'li'
+console.log(n.name)
 </script>
 
