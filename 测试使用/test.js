c9ed69c79248c7ac253ef111f7d29b3261be7ffa
goddessIU <2581606394@qq.com>
2022-04-07 21:38:34 +0800

    4.7-1

--- 测试使用/test.js
+++ 测试使用/test.js
@@ -1,1 +1,12 @@
-console.log(process.memoryUsage())
+
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+const promise4 = 2
+
+Promise.allSettled([promise1, promise2, promise3, promise4]).then(val => {
+    console.log(val)
+})
