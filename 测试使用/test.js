8b9affb103b2b24f8fd785e728ad56c29356d8e7
goddessIU <2581606394@qq.com>
2022-04-10 16:15:51 +0800

    4.10-1

--- 测试使用/test.js
+++ 测试使用/test.js
@@ -1,33 +1,17 @@
-Promise.allSettled2 = function(arrs) {
-    let res = []
-    let count = 0
-    return new Promise((resolve, reject) => {
-        arrs.forEach((p, i) => {
-            Promise.resolve(p).then(val => {
-                res[i] = {
-                    status: 'fulfilled',
-                    value: val
-                }
-                count++
-                if (count === arrs.length) {
-                    resolve(res)
-                }
-            }, rea => {
-                res[i] = {
-                    status: 'rejected',
-                    reason: rea
-                }
-                count++
-                if (count === arrs.length) {
-                    resolve(res)
-                }
-            })
-        })
-    })
+var fn = null;
+function foo() {
+    var a = 2;
+    function innnerFoo() { 
+        console.log(c); 
+        console.log(a);
+    }
+    fn = innnerFoo; 
 }
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
-});
-Promise.allSettled2([promise1, promise2, promise3]).then(console.log)
+
+function bar() {
+    var c = 100;
+    fn(); 
+}
+
+foo();
+bar();
--- 测试使用/test1.js
+++ 测试使用/test1.js
@@ -1,25 +1,22 @@
-function clone(target, map = new WeakMap()) {
-    if (typeof target === 'object') {
-        let cloneTarget = Array.isArray(target) ? [] : {}
-        if (map.get(target)) {
-            return target
-        } 
-        map.set(target, cloneTarget)
-        for (const key in target) {
-            cloneTarget[key] = clone(target[key], map)
-        }
-        return cloneTarget
-    } else {
-        return target
-    }
+Promise.resolve2 = function(p) {
+    if (p instanceof Promise) return p
+    return Promise.resolve(p)
 }
-const target = {
-    field1: 1,
-    field2: undefined,
-    field3: {
-        child: 'child'
-    },
-    field4: [2, 4, 8]
-};
-target.target = target;
-console.log(clone(target))
+Promise.resolve2(2).then(val => console.log(val))
+
+// let p1 = new Promise((res, rej) => {
+//     setTimeout(res(1), 3000)
+// })
+// let p2 = 2
+// let p3 = Promise.resolve(3)
+// let p4 = new Promise((res, rej) => {
+//     rej(4)
+// })
+// let p5 = new Promise(() => {
+
+// })
+// Promise.all2([p1, p2, p3,p4,  p5]).then(val => {
+//     console.log(val)
+// }, err => {
+//     console.log(err)
+// })
