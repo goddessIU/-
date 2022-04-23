66b98df1c8779c040379b272832f3d1486033038
goddessIU <2581606394@qq.com>
2022-04-24 00:09:18 +0800

    4.24-1

--- 测试使用/task.js
+++
@@ -1,5 +0,0 @@
-Promise.race([
-  new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
-  new Promise((resolve, reject) => setTimeout(() => reject(new Error("Whoops!")), 1000)),
-  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
-]).then(console.log); // 1
