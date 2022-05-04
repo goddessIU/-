f238e5aba72c6168f08026243e5d27edd44b61e7
goddessIU <2581606394@qq.com>
2022-05-04 23:20:36 +0800

    5.4-3

--- 测试使用/task.js
+++ 测试使用/task.js
@@ -1,7 +1,58 @@
-var a= function P() { this.b =3; }
-var c = new a();
-a.prototype.b = 9;
-var b = 7;
-a();
-console.log(b);
-console.log(c.b);
+function judgeType(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+}
+
+function isObject(obj) {
+    return (typeof obj === 'function' || typeof obj === 'object') && obj !== null
+}
+
+function deepClone(obj, set = new WeakSet()) {
+    if (!isObject(obj)) {
+        return obj
+    }
+    if (set.has(obj)) {
+        return obj
+    }
+    set.add(obj)
+
+    let source = null
+    let type = undefined
+    if (Array.isArray(obj)) type = 'array'
+    else type = judgeType(obj)
+
+    if (type === 'array') {
+        source = []
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                source[key] = deepClone(obj[key], set)
+            }
+        }
+    } else if (type === 'date') {
+        console.log(obj)
+        
+        return new Date(obj)
+    } else {
+        source = Object.create(null)
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                source[key] = deepClone(obj[key], set)
+            }
+        }
+    }
+
+
+    return obj
+}
+
+let target = {
+    a: 1,
+    b: [2, 3],
+    c: {
+        d: 4
+    },
+    e: new Date(0)
+}
+
+target.f = target
+console.log(target.e)
+console.log(deepClone(target))
